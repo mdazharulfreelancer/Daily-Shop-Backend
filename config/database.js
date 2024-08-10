6f38e15343a9600e844068a46bb0e5109b06a609
MD Azharul <150342131+mdazharulfreelancer@users.noreply.github.com>
2024-08-10 14:37:24 +0600

    Update database.js

--- config/database.js
+++ config/database.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose")
 const ConnectDataBase = () =>{
     mongoose.set("strictQuery", false);
     mongoose
-    .connect("mongodb+srv://dailyhop:a58mRJfCYSbJfGhG@dailyshop.4rnf6.mongodb.net/dailyshop")
+    .connect("mongodb+srv://azharul:0HHpaElwgzZHF8k5@azharul.3dooh1i.mongodb.net/DailyBazer")
     .then(()=>{
         console.log("connected DataBase")
     })
